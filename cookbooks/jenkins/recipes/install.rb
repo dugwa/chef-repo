cf637e9c3ee9b46837adaa4b3d93ad98b0787327
akachi ugwa <akachiugwa@yahoo.com>
2016-11-15 16:03:40 +0000

    use cookbook attributes to pin down java and jenkins versions

--- cookbooks/jenkins/recipes/install.rb
+++ cookbooks/jenkins/recipes/install.rb
@@ -1,21 +1,25 @@
-cookbook_file '/opt/install_java.sh' do
-  source 'java.sh'
+template '/opt/install_java.sh' do
+  source 'java.sh.erb'
   owner  'root'
   group  'root'
   mode   '755'
 end
 
-cookbook_file '/opt/install_jenkins.sh' do
-  source 'jenkins.sh'
+template '/opt/install_jenkins.sh' do
+  source 'jenkins.sh.erb'
   owner  'root'
   group  'root'
   mode   '755'
 end
+
+Chef::Log.info('installing Java 8')
 
 execute 'install_java' do
   command 'bash /opt/install_java.sh'
   not_if "ps -ef |pgrep java"
 end
+
+Chef::Log.info('installing Jenkins')
 
 execute 'install_jenkins' do
   command 'bash /opt/install_jenkins.sh'
@@ -34,4 +38,10 @@ end
 
 execute 'open jenkins port' do
   command 'firewall-cmd --zone=public --permanent --add-port=8080/tcp && firewall-cmd --reload'
+end
+
+if File.exists?('/usr/lib/jenkins')
+  Chef::Log.info('Jenkins installation complete')
+else
+  Chef::Log.fatal('Jenkins installation failed !!!')
 end
