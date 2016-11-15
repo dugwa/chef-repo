8e077f69917cfdae9f9dd8840ba4c913e2d7d4c1
akachi ugwa <akachiugwa@yahoo.com>
2016-11-15 17:29:07 +0000

    got chef logging working

--- cookbooks/jenkins/recipes/install.rb
+++ cookbooks/jenkins/recipes/install.rb
@@ -12,18 +12,22 @@ template '/opt/install_jenkins.sh' do
   mode   '755'
 end
 
-Chef::Log.info("installing Java 8")
+log 'starting installation of java version 8' do
+  level :info
+end
 
 execute 'install_java' do
   command 'bash /opt/install_java.sh'
   not_if "ps -ef |pgrep java"
 end
 
-Chef::Log.info("installing Jenkins")
+log 'starting installation of jenkins' do
+  level :info
+end
 
 execute 'install_jenkins' do
   command 'bash /opt/install_jenkins.sh'
-  not_if { File.exists? '/usr/lib/jenkins' }
+  not_if { Dir.exists? '/usr/lib/jenkins' }
 end
 
 service 'jenkins.service' do
@@ -40,8 +44,6 @@ execute 'open jenkins port' do
   command 'firewall-cmd --zone=public --permanent --add-port=8080/tcp && firewall-cmd --reload'
 end
 
-if File.exists?('/usr/lib/jenkins')
-  Chef::Log.info("Jenkins installation complete")
-else
-  Chef::Log.fatal("Jenkins installation failed !!!")
+log 'you have just successfully installed jenkins' do
+  level :info
 end
