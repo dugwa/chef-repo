a974c28427f16adcff687c4201480a8dae8cfbfd
akachi ugwa <akachiugwa@yahoo.com>
2016-11-15 16:08:38 +0000

    removing unneeded files

--- cookbooks/jenkins/files/default/jenkins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yum install wget -y
-
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-
-rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
-
-yum --enablerepo=jenkins install jenkins -y
