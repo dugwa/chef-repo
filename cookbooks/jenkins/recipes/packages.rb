65354ebb4592a213d867d3b5ad1babe72c7d2928
chefwork <chefwork@dannyboy.com>
2017-01-17 23:09:14 +0000

    install links

--- cookbooks/jenkins/recipes/packages.rb
+++ cookbooks/jenkins/recipes/packages.rb
@@ -1,7 +1,7 @@
 #install necessary packages
 
 execute 'install_rsync and openssh-clients' do
-  %w(rsync openssh-clients git nc telnet).each do |pkg|
+  %w(rsync openssh-clients git nc telnet links).each do |pkg|
     command "yum install -y #{pkg}"
   end 
 end
