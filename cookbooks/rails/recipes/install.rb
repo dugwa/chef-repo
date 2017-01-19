1559485145efc30df5a0914f925dae584874ab77
chefwork <chefwork@dannyboy.com>
2017-01-19 19:10:06 +0000

    kitchen testing

--- cookbooks/rails/recipes/install.rb
+++ cookbooks/rails/recipes/install.rb
@@ -17,7 +17,7 @@ bash 'jenkins shell login' do
 end
 
 execute 'rails pre-requisites' do
-  %w(git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel).each do |pkgs|
+  %w(git zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel).each do |pkgs|
     command "yum install -y #{pkgs}"
   end
 end  
@@ -27,8 +27,7 @@ bash 'install nodejs' do
   code <<-EOH
   cd /opt
   wget http://nodejs.org/dist/v0.10.30/node-v0.10.30-linux-x64.tar.gz
-  cwd '/opt'
-  sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
+  sudo tar --strip-components 1 -xzvf /opt/node-v0.10.30-linux-x64.tar.gz -C /usr/local
   EOH
 end
 
@@ -40,16 +39,16 @@ bash 'install_rbenv' do
   user 'jenkins'
   code <<-EOH
   cd /var/lib/jenkins
-  git clone git://github.com/sstephenson/rbenv.git .rbenv
-  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  git clone git://github.com/sstephenson/rbenv.git /var/lib/jenkins/.rbenv
+  echo 'export PATH="/var/lib/jenkins/.rbenv/bin:$PATH"' >> ~/.bash_profile
+  echo 'eval "$(rbenv init -)"' >> /var/lib/jenkins/.bash_profile
   exec $SHELL
-  source .bash_profile
-  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+  source /var/lib/jenkins/.bash_profile
+  git clone git://github.com/sstephenson/ruby-build.git /var/lib/jenkins/.rbenv/plugins/ruby-build
+  echo 'export PATH="/var/lib/jenkins/.rbenv/plugins/ruby-build/bin:$PATH"' >> /var/lib/jenkins/.bash_profile
   exec $SHELL
   sleep 5
-  source .bash_profile
+  source /var/lib/jenkins/.bash_profile
   rbenv install -v 2.2.1
   rbenv global 2.2.1
   EOH
