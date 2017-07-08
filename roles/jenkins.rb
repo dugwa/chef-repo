ec7d15366d66d4a5fa829ad0065a3e9b51f0ff66
test-chef-work <test-chef-work@lab.com>
2017-07-08 22:44:26 +0000

    add recipe to tag nodes and also update dockerfiles

--- roles/jenkins.rb
+++ roles/jenkins.rb
@@ -1,3 +1,3 @@
 name "jenkins"
 description "This role contains nodes, which act as jenkins master"
-run_list "recipe[jenkins]", "recipe[jenkins::packages]"
+run_list "recipe[jenkins]", "recipe[jenkins::packages]", "recipe[jenkins::tags]"
